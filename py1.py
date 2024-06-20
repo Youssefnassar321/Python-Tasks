5baa9132d917535b2213287d4ae6881d96d6ba74
Youssefnassar321 <Youssefnassar321@gmail.com>
2024-06-20 08:18:37 +0300

    final

---
+++ py1.py
@@ -0,0 +1,95 @@
+#1
+x = True
+print(type (x))
+
+#2
+y = 2
+print(type (y))
+
+#3
+z = 2.4
+print(type (z))
+
+#4
+s = "youssef"
+print(type (s))
+
+#5
+k = float(y) 
+print(type (k))
+
+#6- Yes 
+t = "234"
+o = int (t)
+print(type(o))
+
+#7
+u = [1,2,3,4,5]
+print(u)
+print (type(u))
+
+#8
+e = (10,11,12,13,14,15)
+print(e)
+print(type(e))
+
+#9
+w = tuple(u)
+print (w)
+print(type(w))
+
+#10
+q = {'name': 'Youssef' , 'age': 22 , 'job': 'Django Develober'}
+print(q)
+print(type(q))
+
+#11- Yes But It Is Not Necessary
+print("youssef");
+print("nassar");
+
+#12- Interpreted
+
+#13
+x = 10
+
+#14
+z = x + 5
+
+#15
+v = x / 5
+
+#16
+d = x * 10
+
+#17
+f = x % 3
+
+#18
+print(11 % 4)
+
+#19
+print(2**3)
+
+#20
+c = 11 / 3
+
+#21
+print(11 // 3)
+
+#22
+r = 10
+if r > 15 :
+    print("r is greater than 15")
+else :
+    print("r is less than 15")
+
+#23
+e = 66
+massage = "e is less than 10"  if e < 10  else "e is more than 10" 
+print(massage)
+
+
+
+
+
+
